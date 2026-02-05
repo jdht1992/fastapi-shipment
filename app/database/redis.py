1ba6048da50099509eddbb80d6ce88a8dc38045b
diego herrera <diego.herrera@broxia-int.com>
2026-02-05 11:25:03 -0600

    added types-passlib in requirements

--- app/database/redis.py
+++ app/database/redis.py
@@ -14,5 +14,5 @@ async def add_jti_to_blacklist(jti: str):
     await _token_blacklist.set(jti, "blacklisted")
 
 
-async def is_jti_blacklisted(jti: str) -> bool:
+async def is_jti_blacklisted(jti: str):
     return await _token_blacklist.exists(jti)
